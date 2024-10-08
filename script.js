b4a9972f4e157e4ab8b7202dada5eb42d3a96877
Alexandra <whitetomatoo@gmail.com>
2024-10-08 16:15:38 +1000

    adapting the code in js-file

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const mainButton = document.getElementById('main_button');
-const changeButton = document.getElementById('change_button');
-
-changeButton.addEventListener('click', () => {
+function changeAnotherElement(){
+    const mainButton = document.getElementById('main_button');
+    const changeButton = document.getElementById('change_button');
     changeButton.classList.toggle('changed'); // Добавление/удаление класса
-});
+}
