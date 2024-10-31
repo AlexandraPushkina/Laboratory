9489bddfaaa17145b3c2c546313624bb4e1f9c34
Alexandra <whitetomatoo@gmail.com>
2024-10-31 17:21:03 +1000

    fix problems

--- laboratory 3/script.js
+++ laboratory 3/script.js
@@ -1,30 +1,29 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-    'use strict'
+    'use strict';
   
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
+    const forms = document.querySelectorAll('.needs-validation');
+    
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
 
-        console.log('test');
-
-        const emailInput = document.getElemenetByID('InputEmail')
-        const passwordInput = document.getElemenetByID('InputPassword');
-        const numberInput = document.getElemenetByID('InputNumber');
-        const ageInput = document.getElemenetByID('InputAge');
+        const emailInput = document.getElementById('InputEmail');
+        const passwordInput = document.getElementById('InputPassword');
+        const numberInput = document.getElementById('InputNumber');
+        const ageInput = document.getElementById('InputAge');
         const regexPatternEmail =/(?:[a-z0-9!#$%&*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&*+/=?^_{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gm;      
         const regexPatternNumber = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{10}$/gm;
 
         let isValidEmail = regexPatternEmail.test(emailInput.value);
-        let isValidPassword = passwordInput.value.length > 4;
+        let isValidPassword = passwordInput.value.length > 3;
         let isValidNumber = regexPatternNumber.test(numberInput.value);
         let isValidAge = parseInt(ageInput.value) >= 18 && parseInt(ageInput.value) <= 90;
-
+        
         if ( !isValidEmail || !isValidPassword || !isValidNumber || !isValidAge )  {
-            event.preventDefault()
+            alert('stop Default complete');
+            event.preventDefault();
             event.stopPropagation();
             if (!isValidEmail){
                 emailInput.classList.add('is-invalid');
@@ -44,7 +43,7 @@
             }
         }
         else
-            form.classList.add('was-validated')
-      }, false)
+            form.classList.add('was-validated');
+      }, false);
     });
   })();
