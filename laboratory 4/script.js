15cce5d57a5a5e447db9c36f7b97a44cb6fba6d0
Alexandra <whitetomatoo@gmail.com>
2024-11-01 21:25:01 +1000

    add parametr 'q'

--- laboratory 4/script.js
+++ laboratory 4/script.js
@@ -1,36 +1,18 @@
 const baseUrl = 'https://api.edamam.com/api'; // Замените на ваш Endpoint 1
 const idRecipe = 'd48672a8';
 const keyRecipe = 'e7d7b3410a7e1f38145f03ff2f29eb78';
-//const baseUrl = 'https://api.balldontlie.io/v1'; // Замените на ваш Endpoint 1
 const dataTable = document.getElementById('dataTable');
-
-function fetchData() {
-    fetch(baseUrl)
-        .then(response =>    {
-            if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => {
-          // Display data in an HTML element
-          outputElement.textContent = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    }
 
 document.getElementById('button').addEventListener('click', () => fetchData(baseUrl));
 
-
-async function fetchData2() {
-  const url = baseUrl+ `/recipes/v2?type=public&app_id=${idRecipe}&app_key=${keyRecipe}` ;
-  let response = await fetch(url);
-  console.log(response.json());
+async function fetchData() {
+  const q = 'q=chicken';
+  const url = baseUrl+ `/recipes/v2?type=public&${q}&app_id=${idRecipe}&app_key=${keyRecipe}` ;
+  let response = await fetch(url).then(response => response.json());
+  console.log(response);
   }
 
-fetchData2();
+fetchData();
 //document.getElementById('button').addEventListener('click', () => fetchData(baseUrl));
 
 
