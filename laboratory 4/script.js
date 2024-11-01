3ce6ea386a2c086b21b70a2161a21ec9cd2fbfab
Alexandra <whitetomatoo@gmail.com>
2024-11-01 21:09:51 +1000

    get Request Success (200)

--- laboratory 4/script.js
+++ laboratory 4/script.js
@@ -1,8 +1,11 @@
-const apiUrl = 'https://api.edamam.com/api/recipes/v2'; // Замените на ваш Endpoint 1
+const baseUrl = 'https://api.edamam.com/api'; // Замените на ваш Endpoint 1
+const idRecipe = 'd48672a8';
+const keyRecipe = 'e7d7b3410a7e1f38145f03ff2f29eb78';
+//const baseUrl = 'https://api.balldontlie.io/v1'; // Замените на ваш Endpoint 1
 const dataTable = document.getElementById('dataTable');
 
 function fetchData() {
-    fetch(apiUrl)
+    fetch(baseUrl)
         .then(response =>    {
             if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -18,5 +21,16 @@ function fetchData() {
         });
     }
 
-document.getElementById('button').addEventListener('click', () => fetchData(apiUrl));
+document.getElementById('button').addEventListener('click', () => fetchData(baseUrl));
+
+
+async function fetchData2() {
+  const url = baseUrl+ `/recipes/v2?type=public&app_id=${idRecipe}&app_key=${keyRecipe}` ;
+  let response = await fetch(url);
+  console.log(response.json());
+  }
+
+fetchData2();
+//document.getElementById('button').addEventListener('click', () => fetchData(baseUrl));
+
 
