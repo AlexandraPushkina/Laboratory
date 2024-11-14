81aeb96b21114862c630046fc4caecab2793fdc8
Alexandra <whitetomatoo@gmail.com>
2024-11-14 12:19:47 +1000

    add history, but appear bugs with restart or win-condition

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,90 +2,106 @@ import { useState } from 'react';
 
 import './App.css';
 
-function App() {
-  return(
-    <div className="App">
-      <Board/>
-    </div>
-  );
-}
+// function App() {
+//   return(
+//     <div className="App">
+//       <Board/>
+//     </div>
+//   );
+// }
 
 function Square({ value, onSquareClick }) // value и onSquareClick - это props(свойства этого Square)
 {
   return (
     <button className='square' onClick={onSquareClick}>{ value }</button>  
-  )
+  );
 }
 
-function Board()
-{
-  const [onPlay, setOnPlay] = useState(true); // Хук, который меняет значение состояния возможности игры
-  const [whoIsNext, setNext] = useState(true) // Хук, который меняет значение переменной налету без обновления страницы
+function Board({ xIsNext, squares, onPlay }){
+  //const [onPlay, setOnPlay] = useState(true); // Хук, который меняет значение состояния возможности игры
   const [gameStatus, setGameStatus] = useState(true); // Хук, который меняет состояние строки вывода игры
-  const [squares, setSquares] = useState(new Array(9).fill('')); // Хук, который меняет массив крестиков и ноликов, т.к. нам нужно его обнулять после рестарта
+  // function checkWinner(squares)
+  // {
+  //   const winLines = [
+  //     [0, 1, 2],
+  //     [3, 4, 5],
+  //     [6, 7, 8],
+  //     [0, 3, 6],
+  //     [1, 4, 7],
+  //     [2, 5, 8],
+  //     [0, 4, 8],
+  //     [2, 4, 6]
+  //   ];
 
-  function checkWinner(squares)
-  {
-    const winLines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
+  //   for(let i = 0; i < winLines.length; i++)
+  //   {
+  //      const [a, b, c] = winLines[i];
+  //      if(squares[a] === squares[b] && squares[a] === squares[c] && squares[a] !== '')
+  //         return squares[a];
+  //   }
 
-    for(let i = 0; i < winLines.length; i++)
-    {
-       const [a, b, c] = winLines[i];
-       if(squares[a] === squares[b] && squares[a] === squares[c] && squares[a] !== '')
-          return squares[a];
-    }
-
-    if (!squares.includes(''))
-    {
-       restartGame();
-       return 'Nobody';
-    }
-    return null;
-  }
+  //   if (!squares.includes(''))
+  //   {
+  //      restartGame();
+  //      return 'Nobody';
+  //   }
+  //   return null;
+  // }
+  
 
   function onSquareClick(i){
-    if (squares[i] === '' && onPlay)
-    {
-      if (whoIsNext)
-      {
-        squares[i] = 'X';
-        setNext(false);
-      }
-      else
-      {
-        squares[i] = 'O';
-        setNext(true);
-      }
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+    onPlay = false;
+    setGameStatus(winner + ' is win this game');
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+    //if (squares[i] === '' && onPlay)
+    //{
+      // if (xIsNext){
+      //   squares[i] = 'X';
+      //   setXIsNext(false);
+      // }
+      // else{
+      //   squares[i] = 'O';
+      //   setXIsNext(true);
+      // }
+    //}
     
-    afterClick();
-  }
+    //onPlay(nextSquares);
+    //afterClick();
+  //}
 
-  function afterClick()
-  {
-    const winner = checkWinner(squares);
-    if (winner !== null && winner !== 'Nobody')
-    {
-      setOnPlay(false);
-      setGameStatus(winner + ' is win this game');
-    }
-    else
-      setGameStatus('Next turn is: ' + (whoIsNext ? 'O' : 'X'));
-  }
+  // function afterClick()
+  // {
+  //   const winner = checkWinner(squares);
+  //   if (winner !== null && winner !== 'Nobody')
+  //   {
+  //     setOnPlay(false);
+  //     setGameStatus(winner + ' is win this game');
+  //   }
+  //   else
+  //     setGameStatus('Next turn is: ' + (xIsNext ? 'O' : 'X'));
+  // }
 
   function restartGame()
   {
-    setOnPlay(true);
-    setSquares(new Array(9).fill(''));
+    onPlay = true;
+    //squares = useState([Array(9).fill(null)]);
     setGameStatus('');
   }
 
@@ -111,4 +127,68 @@ function Board()
     </>
   );
 }
-export default App;
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+//export default App;
